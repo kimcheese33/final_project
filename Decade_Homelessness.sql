58714d2fa73427f72c8609ae743ff89c09d42527
Kimberly Horany <kim_horany@Kimberlys-MBP.attlocal.net>
2021-04-07 14:17:06 -0500

    removing gitignore files

--- Decade_Homelessness.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE "Decade_Homelessness" (
-    "Year" VARCHAR   NOT NULL,
-    "State" TEXT   NOT NULL,
-    "CoC_Number" VARCHAR   NOT NULL,
-    "CoC_Name" VARCHAR   NOT NULL,
-    "Measures" TEXT   NOT NULL,
-    "Count" INT   NOT NULL
-);
